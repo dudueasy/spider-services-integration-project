fd8b35b2cbb1c4faa2692f65ae8ea06f570b68fb
dudueasy <dudueasy@126.com>
2019-01-24 04:34:01 +0800

    为存储微服务信息创建了model

---
+++ models/mongoose/spider_servcies.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+let spiderSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    index: true,
+    unique: true,
+  },
+  validationUrl: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: [
+      "registered",
+      "validated",
+      "running",
+      "paused",
+      "stopped",
+      "up_to_date",
+    ],
+  },
+  singleContent: {
+    url: String,
+    frequency: Number,
+  },
+  contentList: {
+    url: {
+      type: String,
+      required: true,
+    },
+    frequency: {
+      type: Number,
+      default: 10,
+    },
+    pageSize: {
+      type: Number,
+      default: 10,
+    },
+  },
+});
+
+const SpiderModel = mongoose.model('SpiderServices', spiderSchema);
+module.exports = SpiderModel
+
