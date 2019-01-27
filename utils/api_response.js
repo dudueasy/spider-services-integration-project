7a95fa08c9d4f1098115034dd80f5c5f5b7ec859
dudueasy <dudueasy@126.com>
2019-01-28 01:12:44 +0800

    优化代码逻辑 && bug fixes

--- utils/api_response.js
+++ utils/api_response.js
@@ -1,19 +1,23 @@
+let logger = require('./loggers/logger');
+
 module.exports = (req, res) => {
   const {data, err} = req;
-  console.log('apiRes receive data from req.data: ', JSON.stringify(data));
 
   if (err) {
+    logger('error', 'uncaughtException error: %s', err.message, err.stack);
     res.render('error', {message: err.message, error: err});
   }
+  else {
+    console.log('apiRes receive data from req.data: ', JSON.stringify(data));
+    const responseData = {
+      code: 0,
+      data: req.data,
+    };
 
-  const responseData = {
-    code: 0,
-    data: req.data,
-  };
+    if (data.token) {
+      res.cookie('jwt-token', JSON.stringify(data.token));
+    }
 
-  if (data.token) {
-    res.cookie('jwt-token', JSON.stringify(data.token));
+    res.json(responseData);
   }
-
-  res.json(responseData);
 };
