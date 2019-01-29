4f6dfab9eda84ca307b122e7cde72549dc788301
dudueasy <dudueasy@126.com>
2019-01-30 07:37:26 +0800

    为来自爬虫微服务的数据创建model 和 schema

---
+++ models/mongoose/spider_services_content_model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+const {ObjectId} = Schema.Types;
+
+const ContentSchema = new Schema({
+  spiderServiceContentId: String, // resource _id in it's spider service mongodb database
+  spiderServiceId: {
+    type: ObjectId,
+    index: true,
+    required: true,
+  },
+  content: {
+    resourceId: String, // resource id (articleId)
+    articleContentHtml: String,
+    articleContentText: Object,
+    createdAt: {type: String, default: new Date(Date.now().valueOf())},
+    originalCreatedAt: String,
+  },
+  title: {type: String, required: true},
+  tags: [{name: String, value: String, score: Number}],
+});
+
+const ContentModel = mongoose.model('serviceContent', ContentSchema);
+module.exports = ContentModel;
