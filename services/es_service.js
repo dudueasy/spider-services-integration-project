228e2eb56e6254d5986990e8f4c65046ff21d36b
dudueasy <dudueasy@126.com>
2019-03-25 22:01:31 +0800

    添加了 elastic 服务模块

---
+++ services/es_service.js
@@ -0,0 +1,71 @@
+const ElasticSearch = require('elasticsearch');
+const path = require('path');
+require('dotenv').config({path: path.join(__dirname, "../.env")});
+
+let esHost = process.env.ES_HOST;
+let esIndex = process.env.ES_INDEX;
+let esType = process.env.ES_TYPE;
+
+// build elasticsearch connection
+let client = new ElasticSearch.Client({
+  host: esHost,
+});
+
+async function initElasticSearch() {
+  await client.indices.putMapping({
+    index: esIndex,
+    type: esType,
+    body: {
+      properties: {
+        id: {type: "text"},
+        title: {type: "text"},
+        serviceId: {type: "text"},
+        tags: {
+          type: "nested",
+          properties: {
+            tagValue: {
+              "type": "text",
+            },
+            tagScore: {
+              "type": "float",
+            },
+          },
+        },
+      },
+    },
+  });
+}
+
+
+// insert spider fetched content into local elasticsearch db
+async function createOrUpdateContent(content) {
+
+  const doc = {
+    title: content.title,
+    tags: content.tags,
+    serviceId: content.spiderServiceId,
+  };
+
+  await client.update({
+    index: esIndex,
+    type: esType,
+    id: (content._id.toString()),
+    body: {
+      doc,
+      "doc_as_upsert": true,
+    },
+  });
+}
+
+async function createOrUpdateContentList(contentList) {
+  let ps = [];
+  for (let content of contentList) {
+    ps.push(createOrUpdateContent(content));
+  }
+
+  await Promise.all(ps);
+}
+
+module.exports = {createOrUpdateContent, createOrUpdateContentList};
+
+
